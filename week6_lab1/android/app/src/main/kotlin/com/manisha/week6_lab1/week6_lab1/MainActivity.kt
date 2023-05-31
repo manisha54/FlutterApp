088760883ed3d5282acb4d92dee7fe32c168ecd4
manisha chaudhary <manishachaudhary2057@gmail.com>
2023-05-31 19:21:05 +0545

    week 6 of lab1 done

---
+++ week6_lab1/android/app/src/main/kotlin/com/manisha/week6_lab1/week6_lab1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.manisha.week6_lab1.week6_lab1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ week6_lab1/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:week6_lab1/di/di.dart';
+
+import 'app.dart';
+
+void main() {
+  initModule();
+  runApp(const App());
+}
